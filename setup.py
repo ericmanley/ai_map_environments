3f662730249029e5ec7021d4014ab450d0ffb7f0
Eric Manley <ericm@thirdcoastcommodities.com>
2024-02-26 11:10:07 -0600

    incrementing version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mapbots',
-    version='0.1.5',  
+    version='0.1.6',  
     author='Eric Manley',
     author_email='eric.manley@drake.edu',
     description='tools for making softbots that operate on maps',
